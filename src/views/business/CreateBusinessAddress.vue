b933d0254f8d3caa5e0f031d802e00dda49cd358
Uttam Patel <uttam11.velani11@gmail.com>
2022-11-05 22:14:48 +0530

    changed app name

--- src/views/business/CreateBusinessAddress.vue
+++ src/views/business/CreateBusinessAddress.vue
@@ -2,7 +2,7 @@
     <ion-page>
         <ion-header>
             <ion-toolbar>
-                <ion-title>MAA BUSINESS</ion-title>
+                <ion-title>proffer</ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
